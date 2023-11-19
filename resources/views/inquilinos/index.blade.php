e053805df043e6ce4ae9f3807eb3481adb979b23
Boris Bonett <borisbonett@Boriss-MacBook-Pro.local>
2023-11-19 18:50:59 -0500

    [Add] filter for code in inquilinos

--- resources/views/inquilinos/index.blade.php
+++ resources/views/inquilinos/index.blade.php
@@ -6,12 +6,21 @@
     </x-slot>
     <div class="py-4">
         <div class="max-w-5xl mx-auto sm:px-4 lg:px-8">
-            <div class="m-0 p-0">
-            <a href="{{ url('/inquilinos/create') }}">
-                <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full">
-                Crear Nuevo Inquilino
-                </button>
-            </a>
+            <div class="">
+                <div class="my-4">
+                    <label class="relative block">
+                        <span class="sr-only">Search</span>
+                        <span class="absolute inset-y-0 left-0 flex items-center pl-2">
+                            <svg class="h-5 w-5 fill-slate-300" viewBox="0 0 20 20"><!-- ... --></svg>
+                        </span>
+                        <input id="identificationFilter" class="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Buscar por indetificación" type="text" name="search"/>
+                    </label>
+                </div>
+                <a href="{{ url('/inquilinos/create') }}">
+                    <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full">
+                    Crear Nuevo Inquilino
+                    </button>
+                </a>
             </div>
         </div>
     </div>
@@ -182,6 +191,28 @@ function validateString(string){
     const joinedString = splitString.join("");
     return joinedString;
 }
+
+// Obtener el campo de búsqueda por su ID
+const identificationFilterInput = document.getElementById('identificationFilter');
+
+// Escuchar eventos de entrada en el campo de búsqueda
+identificationFilterInput.addEventListener('input', function() {
+    // Obtener el valor ingresado en el campo de búsqueda
+    const identification = this.value.toLowerCase().trim();
+
+    // Obtener todas las filas de la tabla
+    const rows = document.querySelectorAll('#row');
+
+    // Iterar sobre las filas y mostrar/ocultar según la identificación ingresada
+    rows.forEach(row => {
+        const identificationColumn = row.querySelector('th:first-child');
+        if (identificationColumn.textContent.toLowerCase().includes(identification)) {
+            row.style.display = ''; // Mostrar la fila si coincide con la identificación
+        } else {
+            row.style.display = 'none'; // Ocultar la fila si no coincide con la identificación
+        }
+    });
+});
 
 </script>
 </x-app-layout>
